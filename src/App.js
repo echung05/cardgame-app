6fc98ca223b324cbc1a3621f82e2f6ca24d70cb3
Esther Chung <e.chung059@gmail.com>
2020-05-26 17:32:18 -0400

    Added Favicon, Comments, Began compareCards

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ const Deck = [
   '8H', '8C', '8S', '8D',
   '9H', '9C', '9S', '9D',
   '10H', '10C', '10S', '10D',
-  'HH', 'JC', 'JS', 'jD',
+  'JH', 'JC', 'JS', 'JD',
   'QH', 'QC', 'QS', 'QD',
-  'KH', 'KC', 'KS', 'kD',
+  'KH', 'KC', 'KS', 'KD',
   'AH', 'AC', 'AS', 'AD',
 ]
 
@@ -31,6 +31,7 @@ class App extends Component {
       cpuCardPlayed: [],
       pCards: [],
       pCardPlayed: [],
+      winnableCards: [],
       inGame: false
     };
 
@@ -38,7 +39,9 @@ class App extends Component {
 
   handleStart = () => {
     console.log("starting game");
+    // shuffle deck
     let shuffledDeck = this.shuffleCards();
+    // distribute cards
     for (let i = 0; i < shuffledDeck.length; i++) {
       if (i % 2 === 0) {
         this.state.cpuCards.push(shuffledDeck[i]);
@@ -50,39 +53,66 @@ class App extends Component {
     this.setState({ inGame: true, });
   }
 
+  // helps shuffle cards before dealing 
   shuffleCards = () => {
     console.log("shuffling!");
     let index = Deck.length, temp, randIndex;
+    // while not at the first card in deck
     while (0 !== index) {
       randIndex = Math.floor(Math.random() * index);
       index -= 1;
-
+      // switch spots of card at end of deck and card in random spot in deck
       temp = Deck[index];
       Deck[index] = Deck[randIndex];
       Deck[randIndex] = temp;
     }
     return Deck;
   }
-
+  // draws and compares cards, handles matching card draws in compareCards()
   handleDraw = () => {
     console.log("drawing!");
+    this.drawCards();
+    this.compareCards();
+  }
+
+  drawCards = () => {
     // deal CPU card
     this.state.cpuCardPlayed.push(this.state.cpuCards[0]);
     const newCpuDeck = this.state.cpuCards.slice(1);
-    console.log(newCpuDeck.length);
     this.setState(prevState => {
       return { cpuCards: newCpuDeck };
     })
-    console.log(this.state.cpuCardPlayed);
+    console.log("cpu card played ", this.state.cpuCardPlayed);
     // deal player card
     this.state.pCardPlayed.push(this.state.pCards[0]);
     const newPDeck = this.state.pCards.slice(1);
-    console.log(newPDeck.length);
     this.setState(prevState => {
       return { pCards: newPDeck };
     })
-    console.log(this.state.pCardPlayed);
+    console.log("player card played ", this.state.pCardPlayed);
   }
+
+  compareCards = () => {
+    let cpuCardVal = this.state.cpuCardPlayed[0].charAt(0);
+    let pCardVal = this.state.pCardPlayed[0].charAt(0);
+    let currCpuCard = this.state.cpuCardPlayed[0];
+    let currPCard = this.state.pCardPlayed[0];
+    console.log("cpu card value ", cpuCardVal, "| player card value ", pCardVal);
+    //in the event of a tie...
+    if (cpuCardVal === pCardVal) {
+      //each player puts current card on winnable deck
+      this.state.winnableCards.push(currCpuCard);
+      this.state.winnableCards.push(currPCard);
+      // each player draws next card placed face up
+      this.drawCards();
+      // each player draws next card placed face down
+      this.drawCards();
+      console.log("winnable: ", this.state.winnableCards);
+      console.log("drawn: ", this.cpuCardPlayed, this.pCardPlayed);
+    }
+
+  }
+
 
   render() {
 
@@ -104,21 +134,31 @@ class App extends Component {
     }
     let cpuCard;
     if (inGame) {
-      cpuCard = this.state.cpuCardPlayed;
+      cpuCard = this.state.cpuCardPlayed[0];
     } else {
       cpuCard = "CARD";
     }
 
     let pCard;
     if (inGame) {
-      pCard = this.state.pCardPlayed;
+      pCard = this.state.pCardPlayed[0];
     } else {
       pCard = "CARD";
     }
 
+
     return (
       <div className="App">
         <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">
+            <img
+              src="https://image.flaticon.com/icons/png/512/35/35203.png"
+              width="40"
+              height="40"
+              className="d-inline-block align-top"
+              alt="icon"
+            />
+          </Navbar.Brand>
           <Navbar.Brand href="#home">War</Navbar.Brand>
         </Navbar>
 
